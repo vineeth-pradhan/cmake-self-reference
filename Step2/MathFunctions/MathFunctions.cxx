7484a3d06af281bd3533faf7f0427bccd4c73a87
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2025-01-31 13:25:56 +0530

    Adding option to pass OPTION in shell script

--- Step2/MathFunctions/MathFunctions.cxx
+++ Step2/MathFunctions/MathFunctions.cxx
@@ -15,7 +15,6 @@ double sqrt(double x)
   // TODO 9: If USE_MYMATH is defined, use detail::mysqrt.
   // Otherwise, use std::sqrt.
 #ifdef USE_MYMATH
-  std::cout << USE_MYMATH << "\n";
   std::cout << "Using custom sqrt functions \n";
   return detail::mysqrt(x);
 #else
--- Step2/build.sh
+++ Step2/build.sh
@@ -3,7 +3,13 @@ clear
 echo "Removing build directory.....🧹"
 rm -rf build
 echo "Building with cmake.....🏗️"
-cmake -Bbuild -S.
+if [ "$1" == "USE_MYMATH=ON" ]; then
+  echo "Enabling USE_MYMATH OPTION....."
+  cmake -Bbuild -S. -DUSE_MYMATH=ON
+else
+  echo "Building without USE_MYMATH....."
+  cmake -Bbuild -S.
+fi
 echo "Running make cmd.....💨"
 make -Cbuild
 echo "Done....🔥"
