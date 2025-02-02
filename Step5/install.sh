1ec29fec55c389ae5431137fae2fc9f200df677d
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2025-02-02 21:41:56 +0530

    Install script

---
+++ Step5/install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Installing with cmake.....🛠️"
+cmake --install build/. --prefix /Users/vineeth/Practice/cmake-installs/Tutorial 
+echo "Running a sample test 🧪"
+/Users/vineeth/Practice/cmake-installs/Tutorial/bin/Tutorial 9
+echo "Done....🔥"
