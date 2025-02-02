cca49b76a22b10863f7021c6c16c7666abcaf439
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2025-02-02 21:43:46 +0530

    Prompt improvements

--- Step5/install.sh
+++ Step5/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo "Installing with cmake.....🛠️"
 cmake --install build/. --prefix /Users/vineeth/Practice/cmake-installs/Tutorial 
-echo "Running a sample test 🧪"
+echo "Running a sample test.....🧪"
+echo "Installed dir: /Users/vineeth/Practice/cmake-installs/Tutorial/bin/Tutorial"
 /Users/vineeth/Practice/cmake-installs/Tutorial/bin/Tutorial 9
 echo "Done....🔥"
