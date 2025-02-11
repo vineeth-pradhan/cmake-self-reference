060c9392a0d7cc7d35afe0fedbe4f452426615d4
Vineeth Pradhan <vineeth.pradhan@gmail.com>
2025-02-11 13:29:09 +0530

    Fix for failing LC_RPATH

--- Step11/install.sh
+++ Step11/install.sh
@@ -2,7 +2,7 @@
 echo "Cleaning up existing installation(if any)..... 🧹"
 rm -rf /Users/vineeth/Practice/cmake-installs/Tutorial/*
 echo "Installing with cmake.....🛠️"
-cmake --install build/. --prefix /Users/vineeth/Practice/cmake-installs/Tutorial 
+CMAKE_INSTALL_PREFIX=/Users/vineeth/Practice/cmake-installs/Tutorial cmake --install build/. --prefix /Users/vineeth/Practice/cmake-installs/Tutorial
 echo "Running a sample test.....🧪"
 echo "Installed dir: /Users/vineeth/Practice/cmake-installs/Tutorial/bin/Tutorial"
 /Users/vineeth/Practice/cmake-installs/Tutorial/bin/Tutorial 9
